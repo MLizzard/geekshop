731be9d4e950a167d99a40bc803e73b1ac7414e3
Максим Марчук <maxmarchuk2006@gmail.com>
2025-02-18 02:36:48 +0300

    Initial commit

---
+++ Geekshop/src/main/java/com/example/geekshop/controllers/ProductController.java
@@ -0,0 +1,42 @@
+package com.example.geekshop.controllers;
+
+import com.example.geekshop.model.Product;
+import com.example.geekshop.service.ProductService;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+    private final ProductService productService = new ProductService();
+
+    @GetMapping
+    public List<Product> findAllProducts() {
+        return productService.findAllProducts();
+    }
+
+    // Найти товар по имени (Query Parameter)
+    @GetMapping("/search")
+    public Product findByName(@RequestParam String name) {
+        Product product = productService.findByName(name);
+        if (product == null) {
+            throw new RuntimeException("Продукт не найден");
+        }
+        return product;
+    }
+
+    // Найти товар по индексу (Path Parameter)
+    @GetMapping("/{index}")
+    public Product findByIndex(@PathVariable int index) {
+        Product product = productService.findByIndex(index);
+        if (product == null) {
+            throw new RuntimeException("Некорректный индекс");
+        }
+        return product;
+    }
+}
